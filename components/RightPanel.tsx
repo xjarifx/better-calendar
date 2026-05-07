a16bf2b42f6355c51e1953c39ab89c78d283119f
Jarif <xjarifx@gmail.com>
2026-05-07 22:17:13 +0600

    refactor: remove unnecessary React import in RightPanel component

--- components/RightPanel.tsx
+++ components/RightPanel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useCalendar } from "@/lib/calendar-context";
 import { useAuth } from "@/lib/auth-context";
 import ExtractedEvents, { ExtractedEvent } from "./ExtractedEvents";
